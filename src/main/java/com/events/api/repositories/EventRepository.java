056736af1d492eaac936eb8b09491664818b1535
Artur Chaves <artur7paiva@gmail.com>
2024-08-25 02:00:47 -0300

    feat: add repositories

---
+++ src/main/java/com/events/api/repositories/EventRepository.java
@@ -0,0 +1,10 @@
+package com.events.api.repositories;
+
+import com.events.api.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventRepository extends JpaRepository<Event, UUID> {
+
+}
